bccaa4f4a70ec62677a253f94bdfb14c9e4ea0d4
Nam Le <namle@minma-266s-MacBook-Air.local>
2023-05-16 15:55:34 +0700

    init package starter kit

---
+++ test/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+const { expect } = require('chai')
+
+const lib = require('../lib')
+
+describe('My package', function () {
+  it('should pass', function () {
+    const answer = lib()
+    expect(answer).to.equals(true)
+  })
+})
